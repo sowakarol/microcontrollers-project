1a5a3cc3c43a59a17ec89ed7db22d1533fb260b3
Bartek Grodek <sher04lock@gmail.com>
2017-05-07 20:38:22 +0200

    Rzeczy do zrobienia na poczatku kodu w komentarzu. Koncze na dzsiaj xD

--- program/program.ino
+++ program/program.ino
@@ -1,35 +1,25 @@
-/*This code is public domain but you buy me a beer if you use this and we meet
-  someday (Beerware license).
-  4 digit 7 segment display:
-  http://www.sparkfun.com/products/9483
-  Datasheet:
-  http://www.sparkfun.com/datasheets/Components/LED/7-Segment/YSD-439AR6B-35.pdf
-  This is an example of how to drive a 7 segment LED display from an ATmega
-  without the use of current limiting resistors. This technique is very common
-  but requires some knowledge of electronics - you do run the risk of dumping
-  too much current through the segments and burning out parts of the display.
-  If you use the stock code you should be ok, but be careful editing the
-  brightness values.
-  This code should work with all colors (red, blue, yellow, green) but the
-  brightness will vary from one color to the next because the forward voltage
-  drop of each color is different. This code was written and calibrated for the
-  red color.
-  This code will work with most Arduinos but you may want to re-route some of
-  the pins.
-  7 segments
-  4 digits
-  1 colon
-  =
-  12 pins required for full control
-*/
+//TODO:
+// 1. segmenty do literek
+// 2. moze jakies posprzatanie kodu, nie wiem czy to bedzie czytelne dla Was :D
+// 3. zmiana jasnosci i predkosci dziala spoczko, ale zmiana tekstu dopiero za drugim razem,
+//    chyba ta funkcja czytaja jest odpalana ze zlego miejsca ale juz nie chce mi sie nad tym kminic dzisiaj
+// 4. have fun
+// 6. obsluga bledow, moze jakies wyswietlanie ERROR wtedy?
+// 7. optymalizacja :3 OSZCZEDZAJ RAM GDZIEKOLWIEK JESTES
+// 8. nie no zart na razie
+// 9. Wisła Płock
+//10. nie ma punktu 5.
 
+
+
+
+
+// PINS CONNECTION
 int digit1 = 11; //PWM Display pin 1
 int digit2 = 10; //PWM Display pin 2
 int digit3 = 9;  //PWM Display pin 6
 int digit4 = 6;  //PWM Display pin 8
 
-//Pin mapping from Arduino to the ATmega DIP28 if you need it
-//http://www.arduino.cc/en/Hacking/PinMapping
 int segA = 2;
 int segB = 3;
 int segC = 4;
@@ -37,6 +27,17 @@ int segD = 5;
 int segE = A0; //pin 6 is used bij display 1 for its pwm function
 int segF = 7;
 int segG = 8;
+//----------------------------------------------------------------
+
+//CROSS-FUNCTION VARIABLES
+int BRIGHTNESS[] = {200, 500, 1000, 2000};
+
+char TEXT[7] = "123456";
+int BRIGHT[4] = {0, 1, 2, 3};
+int DISP = 500;
+
+int buffer[4];
+//----------------------------------------------------------------
 
 void setup()
 {
@@ -52,32 +53,26 @@ void setup()
   pinMode(digit2, OUTPUT);
   pinMode(digit3, OUTPUT);
   pinMode(digit4, OUTPUT);
-  Serial.begin(9600);
-
   pinMode(13, OUTPUT);
+
+  Serial.begin(9600);
 }
-int incomingByte = 0; // for incoming serial data
+//----------------------------------------------------------------
 
-int BRIGHTNESS[] = {200, 500, 1000, 2000};
-
-int BRIGHT[4] = {0, 1, 2, 3};
-int DISP = 500;
-char TEXT[7] = "123456";
-int buffer[4];
 void loop()
 {
-
   doTheReading();
-
   myDisplay(BRIGHT, &DISP, TEXT);
 }
 
 void doTheReading()
 {
+  int incomingByte = 0;
   if (Serial.available() > 0)
   {
-    // read the incoming byte:
     incomingByte = Serial.read();
+    Serial.print("available chars left: ");
+    Serial.println(Serial.available());
 
     switch (incomingByte)
     {
@@ -95,7 +90,7 @@ void doTheReading()
       break;
     //T
     case 84:
-      if (Serial.available() == 6)
+      // if (Serial.available() == 6)
       {
         readAndChangeText();
       }
@@ -103,17 +98,23 @@ void doTheReading()
     }
   }
 }
+//----------------------------------------------------------------
 
 void readAndChangeText()
 {
+  Serial.println("reading new text...");
   char buff[7];
   char c;
   for (int i = 0; i < 6; i++)
   {
-    c = Serial.read();
-    if ((c >= '0'  && c <= '9') || c == 'A' || c == 'b' || c == 'C' || c == 'd' || c == 'E' || c == 'F')
-    {
-      buff[i] = c;
+    buff[i] = Serial.read();
+  }
+
+  for (int i = 0; i < 6; i++)
+  {
+    c = buff[i];
+    if (!((c >= '0' && c <= '9') || c == 'A' || c == 'b' || c == 'C' || c == 'd' || c == 'E' || c == 'F')) {
+      return;
     }
   }
   buff[6] = '\0';
@@ -144,6 +145,8 @@ void changeSpeed()
   DISP = newSpeed;
   Serial.println(DISP);
 }
+//----------------------------------------------------------------
+
 void readBytes()
 {
   Serial.print("In buffer: ");
@@ -153,6 +156,7 @@ void readBytes()
     Serial.print(buffer[i]);
   }
 }
+//----------------------------------------------------------------
 
 void changeBrightness()
 {
@@ -173,6 +177,7 @@ void changeBrightness()
   }
   Serial.println("");
 }
+//----------------------------------------------------------------
 
 void myDisplay(int *brightness, int *display_time, char *text)
 {
@@ -187,6 +192,7 @@ void myDisplay(int *brightness, int *display_time, char *text)
   int nchars = 1;
   for (int i = length + 3; i >= 0; i--)
   {
+    doTheReading();
     if (EMPTY > 0)
     {
       strncpy(substr, spaces, EMPTY);
@@ -206,6 +212,7 @@ void myDisplay(int *brightness, int *display_time, char *text)
     }
   }
 }
+//----------------------------------------------------------------
 
 void displayCharOnDigit(char *toDisplay, int *bright)
 {
@@ -214,7 +221,6 @@ void displayCharOnDigit(char *toDisplay, int *bright)
 #define DIGIT_ON LOW
 #define DIGIT_OFF HIGH
 
-  // int brightness[] = {200, 500, 1000, 2000};
   long beginTime = millis();
   int i = 0;
   for (int digit = 1; digit <= 4; digit++)
@@ -262,25 +268,7 @@ void displayCharOnDigit(char *toDisplay, int *bright)
     ;
   //Wait for 20ms to pass before we paint the display again
 }
-
-//Given a number, we display 10:22
-//After running through the 4 numbers, the display is left turned off
-
-//Display brightness
-//Each digit is on for a certain amount of microseconds
-//Then it is off until we have reached a total of 20ms for the function call
-//Let's assume each digit is on for 1000us
-//Each digit is on for 1ms, there are 4 digits, so the display is off for 16ms.
-//That's a ratio of 1ms to 16ms or 6.25% on time (PWM).
-//Let's define a variable called brightness that varies from:
-//5000 blindingly bright (15.7mA current draw per digit)
-//2000 shockingly bright (11.4mA current draw per digit)
-//1000 pretty bright (5.9mA)
-//500 normal (3mA)
-//200 dim but readable (1.4mA)
-//50 dim but readable (0.56mA)
-//5 dim but readable (0.31mA)
-//1 dim but readable in dark (0.28mA)
+//----------------------------------------------------------------
 
 void displayNumber(int toDisplay)
 {
@@ -332,9 +320,11 @@ void displayNumber(int toDisplay)
     ;
   //Wait for 20ms to pass before we paint the display again
 }
+//----------------------------------------------------------------
 
 //Given a number, turns on those segments
 //If number == 10, then turn off number
+
 void lightNumber(int numberToDisplay)
 {
 
